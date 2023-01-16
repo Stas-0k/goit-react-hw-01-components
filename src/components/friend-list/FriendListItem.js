20c215616f61414d4f130904a5b806da291dfdef
Stas-0k <brickbybrick15@gmail.com>
2023-01-16 16:37:07 +0200

    commit
    commit

--- src/components/friend-list/FriendListItem.js
+++ src/components/friend-list/FriendListItem.js
@@ -4,7 +4,7 @@ import css from './friend-list.module.css'
 export const FriendListItem = ({ avatar, name, isOnline}) => {
     return (
         <li className={css.itemFriend}>
-            <span className={isOnline ? css.online : css.offline}>{isOnline}</span>
+            <span className={isOnline ? css.online : css.offline}></span>
             <img className={css.avatarFriend} src={avatar} alt="User avatar" width="48" />
         <p className="name">{name}</p>
     </li>
@@ -15,5 +15,4 @@ FriendListItem.propTypes = {
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
-    id: PropTypes.number,
 }
